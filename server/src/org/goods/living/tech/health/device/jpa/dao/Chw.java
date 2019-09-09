74e432ec5808aaf10bc4d55f74fbfe6e5fba7952
patriciachebet <pchepkwony@livinggoods.org>
2019-09-09 18:06:33 +0300

    Generated entity classes for the chw table

--- server/src/org/goods/living/tech/health/device/jpa/dao/Chw.java
+++ server/src/org/goods/living/tech/health/device/jpa/dao/Chw.java
@@ -1,16 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.goods.living.tech.health.device.jpa.dao;
-
 import java.io.Serializable;
-import java.util.Collection;
-import java.util.Date;
-
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -18,147 +8,147 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.persistence.UniqueConstraint;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-
-import org.hibernate.annotations.Type;
-import org.hibernate.annotations.TypeDef;
-
-import com.vladmihalcea.hibernate.type.json.JsonNodeBinaryType;
-
 /**
  *
- * @author Chebet
+ * @author ernestmurimi
  */
 @Entity
-@Table(name = "chw", uniqueConstraints = { @UniqueConstraint(columnNames = { "contact_id" }) })
+@Table(name = "chw")
 @XmlRootElement
-@NamedQueries({ @NamedQuery(name = "Chw.findAll", query = "SELECT u FROM Chw u"),
-		@NamedQuery(name = "Chw.findByUsername", query = "SELECT u FROM Chw u WHERE u.username = :username"),
-		@NamedQuery(name = "Chw.findByContactId", query = "SELECT u FROM Chw u WHERE u.contactId = :contactId"),
-		@NamedQuery(name = "Chw.findByChwName", query = "SELECT u FROM Chw u WHERE u.chwName = :chwName"),
-		@NamedQuery(name = "Chw.findByBranchName", query = "SELECT u FROM Chw u WHERE u.branchName = :branchName"),
-		@NamedQuery(name = "Chw.findByChwPhone", query = "SELECT u FROM Chw u WHERE u.chwPhone = :chwPhone"),
-		@NamedQuery(name = "Chw.findByCountry", query = "SELECT u FROM Chw u WHERE u.country = :country"),
-        @NamedQuery(name = "Chw.findBySupervisorName", query = "SELECT u FROM Chw u WHERE u.supervisorName = :supervisorName")
-})
-@TypeDef(name = "jsonb-node", typeClass = JsonNodeBinaryType.class)
+@NamedQueries({
+    @NamedQuery(name = "Chw.findAll", query = "SELECT c FROM Chw c")
+    , @NamedQuery(name = "Chw.findByUsername", query = "SELECT c FROM Chw c WHERE c.username = :username")
+    , @NamedQuery(name = "Chw.findByContactId", query = "SELECT c FROM Chw c WHERE c.contactId = :contactId")
+    , @NamedQuery(name = "Chw.findByChwName", query = "SELECT c FROM Chw c WHERE c.chwName = :chwName")
+    , @NamedQuery(name = "Chw.findByBranchName", query = "SELECT c FROM Chw c WHERE c.branchName = :branchName")
+    , @NamedQuery(name = "Chw.findByChwPhone", query = "SELECT c FROM Chw c WHERE c.chwPhone = :chwPhone")
+    , @NamedQuery(name = "Chw.findByCountry", query = "SELECT c FROM Chw c WHERE c.country = :country")
+    , @NamedQuery(name = "Chw.findBySupervisorName", query = "SELECT c FROM Chw c WHERE c.supervisorName = :supervisorName")
+    , @NamedQuery(name = "Chw.findById", query = "SELECT c FROM Chw c WHERE c.id = :id")})
 public class Chw implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Column(name = "username")
+    private String username;
+    @Basic(optional = false)
+    @Column(name = "contact_id")
+    private String contactId;
+    @Column(name = "chw_name")
+    private String chwName;
+    @Column(name = "branch_name")
+    private String branchName;
+    @Column(name = "chw_phone")
+    private String chwPhone;
+    @Column(name = "country")
+    private String country;
+    @Column(name = "supervisor_name")
+    private String supervisorName;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Long id;
+    
+    public Chw() {
+    }
 
-	private static final long serialVersionUID = 1L;
+    public Chw(Long id) {
+        this.id = id;
+    }
 
-	@Column(name = "username")
-	private String username;
-	@Column(name = "contact_id")
-	private String contactId;
-	@Column(name = "chw_name")
-	private String chwName;
-	@Column(name = "branch_name")
-	private String branchName;
-	@Column(name = "chw_phone")
-	private String chwPhone;
-	@Column(name = "country")
-	private String country;
-	@Column(name = "supervisor_name")
-	private String supervisorName;
-	
+    public Chw(Long id, String contactId) {
+        this.id = id;
+        this.contactId = contactId;
+    }
 
-	public Chw() {
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public Chw(String username) {
-		this.username = username;
-	}
-	
-	public Chw(String Username, String ContactId, String ChwName, String BranchName, String ChwPhone, String Country, String SupervisorName) {
-		this.username = Username;
-		this.contactId = ContactId;
-		this.chwName = ChwName;
-		this.branchName = BranchName;
-		this.chwPhone = ChwPhone;
-		this.country = Country;
-		this.supervisorName = SupervisorName;
-	}
-	
-	public String getUsername() {
-		return username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getContactId() {
-		return contactId;
-	}
+    public String getContactId() {
+        return contactId;
+    }
 
-	public void setContactId(String contactId) {
-		this.contactId = contactId;
-	}
-	
-	public String getChwName() {
-		return chwName;
-	}
+    public void setContactId(String contactId) {
+        this.contactId = contactId;
+    }
 
-	public void setChwName(String chwName) {
-		this.chwName = chwName;
-	}
-	
-	public String getBranchName() {
-		return branchName;
-	}
+    public String getChwName() {
+        return chwName;
+    }
 
-	public void setBranchName(String branchName) {
-		this.branchName = branchName;
-	}
-	
-	public String getChwPhone() {
-		return chwPhone;
-	}
+    public void setChwName(String chwName) {
+        this.chwName = chwName;
+    }
 
-	public void setChwPhone(String chwPhone) {
-		this.chwPhone = chwPhone;
-	}
-	
-	public String getCountry() {
-		return country;
-	}
+    public String getBranchName() {
+        return branchName;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	
-	public String getSupervisorName() {
-		return supervisorName;
-	}
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
 
-	public void setSupervisorName(String supervisorName) {
-		this.supervisorName = supervisorName;
-	}
-	
+    public String getChwPhone() {
+        return chwPhone;
+    }
 
+    public void setChwPhone(String chwPhone) {
+        this.chwPhone = chwPhone;
+    }
 
-	@Override
-	public boolean equals(Object object) {
-		// TODO: Warning - this method won't work in the case the id fields are not set
-		if (!(object instanceof Chw)) {
-			return false;
-		}
-		Chw other = (Chw) object;
-		if ((this.username == null && other.username != null) || (this.username != null && !this.username.equals(other.username))) {
-			return false;
-		}
-		return true;
-	}
+    public String getCountry() {
+        return country;
+    }
 
-	@Override
-	public String toString() {
-		return "org.goods.living.tech.health.device.jpa.dao.Chw[ id=" + username + " ]";
-	}
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
+    public String getSupervisorName() {
+        return supervisorName;
+    }
+
+    public void setSupervisorName(String supervisorName) {
+        this.supervisorName = supervisorName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Chw)) {
+            return false;
+        }
+        Chw other = (Chw) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.goods.living.tech.health.device.jpa.dao.Chw[ id=" + id + " ]";
+    }
+    
 }
